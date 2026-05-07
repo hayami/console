4509687c078b829afddfe63dc4f9fa56757f9e61
HAYAMI <noname@example.com>
2026-05-07 20:17:00 +0900

    rename terminal -> console

--- consoleserver/static/console.js
+++ consoleserver/static/console.js
@@ -32,7 +32,7 @@ window.onload = function () {
   const webLinksAddon = new WebLinksAddon.WebLinksAddon(webLinkHandler);
   term.loadAddon(webLinksAddon);
 
-  const container = document.getElementById("terminal-container");
+  const container = document.getElementById("console-container");
   term.open(container);
   term.focus();
   fitAddon.fit();
