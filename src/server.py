ca2715ba21a112be2306af4c3f9e779e57b7a090
HAYAMI <noname@example.com>
2026-06-24 20:29:47 +0900

    use Middleware, GZipMiddleware

--- src/server.py
+++ src/server.py
@@ -13,6 +13,9 @@ from typing import Any
 
 import socketio
 import starlette.applications
+from starlette.middleware import Middleware
+from starlette.middleware.gzip import GZipMiddleware
+
 
 from . import config
 from . import globalvars as g
@@ -190,6 +193,9 @@ _starlette = starlette.applications.Starlette(
     routes=[
         starlette.routing.Route("/", staticfiles.endpoint),
         starlette.routing.Route("/static/{path:path}", staticfiles.endpoint),
+    ],
+    middleware=[
+        Middleware(GZipMiddleware, minimum_size=1024, compresslevel=6),
     ],
     lifespan=contextlib.asynccontextmanager(_lifespan),
 )
